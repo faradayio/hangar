98fa1cca8c58fc0ad38926f99ffb5dfd27332be7
Andy Rossmeissl <git@rossmeissl.net>
2014-05-21 19:53:34 -0400

    Make sure we're in the test environment

--- lib/hangar.rb
+++ lib/hangar.rb
@@ -2,4 +2,9 @@ require "hangar/engine"
 require 'hangar/constraint'
 
 module Hangar
+  BadEnvironmentError = Class.new(StandardError)
+end
+
+unless Rails.env.test?
+  raise Hangar::BadEnvironmentError
 end
