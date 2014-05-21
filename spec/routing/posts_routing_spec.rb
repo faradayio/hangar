74ce2f3c1286643f2c941e4135f26fb4db536485
Andy Rossmeissl <git@rossmeissl.net>
2014-05-21 16:37:53 -0400

    Failing test to show clobber of app's routes

---
+++ spec/routing/posts_routing_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'Post routing' do
+  it 'provides #create route' do
+    expect(post('/posts')).to route_to('posts#create')
+  end  
+  it 'provides #update route' do
+    expect(patch('/posts/1')).to route_to('posts#update', post_id: 1)
+  end  
+  it 'provides #new route' do
+    expect(get('/posts/new')).to route_to('posts#new')
+  end  
+  it 'provides #show route' do
+    expect(get('/posts/1')).to route_to('posts#show', post_id: 1)
+  end  
+  it 'provides #delete route' do
+    expect(delete('/posts/1')).to route_to('posts#destroy', post_id: 1)
+  end  
+  it 'provides #index route' do
+    expect(get('/posts')).to route_to('posts#index')
+  end  
+end
