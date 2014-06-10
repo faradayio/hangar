c05212fbe65fd437ea9490a227b4e2671e33dd23
Ian Hough <ijhough@gmail.com>
2014-06-10 17:38:12 +0200

    Don't clear the spatial_ref_sys table

--- app/controllers/hangar/records_controller.rb
+++ app/controllers/hangar/records_controller.rb
@@ -3,7 +3,7 @@ module Hangar
     respond_to :json
 
     def delete
-      DatabaseCleaner.clean_with :deletion
+      DatabaseCleaner.clean_with(:deletion, except: %w{ spatial_ref_sys })
       render nothing: true, status: :no_content
     end
   end
