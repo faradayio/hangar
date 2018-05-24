7aa73634b0a2a79b035a3bbae7bbf54beba37fa4
Brad Haydon <bradhaydon@gmail.com>
2018-05-24 12:46:51 -0700

    Add namespace to Hangar factory routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   constraints Hangar::RouteConstraint.new do
+  scope module: 'hangar', path: 'factories', as: 'factory' do
     FactoryBot.factories.map(&:name).map(&:to_s).map(&:pluralize).map(&:to_sym).each do |factory|
       resources factory, only: [:new, :create], controller: 'hangar/resources'
     end
-    delete '/', to: 'hangar/records#delete'
+    delete '/', to: 'records#delete'
   end
 end
