38239d725b54a959af093d697e84b0acda27e8fe
Brad Haydon <bradhaydon@gmail.com>
2018-05-24 18:48:57 -0700

    Add route_namespace global configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   constraints Hangar::RouteConstraint.new do
-  scope module: 'hangar', path: 'factories', as: 'factory' do
-    FactoryBot.factories.map(&:name).map(&:to_s).map(&:pluralize).map(&:to_sym).each do |factory|
-      resources factory, only: [:new, :create], controller: 'hangar/resources'
+    scope Hangar.route_namespace do
+      FactoryBot.factories.map(&:name).map(&:to_s).map(&:pluralize).map(&:to_sym).each do |factory|
+        resources factory, only: [:new, :create], controller: 'resources'
+      end
+      delete '/', to: 'records#delete'
     end
-    delete '/', to: 'records#delete'
   end
 end
--- spec/routing/factory_routing_spec.rb
+++ spec/routing/factory_routing_spec.rb
@@ -12,15 +12,41 @@ describe 'Factory routing' do
     end
   end
 
-  it 'provides #create route' do
-    expect(post: '/factories/posts').to route_to('hangar/resources#create')
+  context 'when route does not have a namespace' do
+    it 'provides #create route' do
+      expect(post: '/posts').to route_to('hangar/resources#create')
+    end
+
+    it 'provides #new route' do
+      expect(get: '/posts/new').to route_to('hangar/resources#new')
+    end
+
+    it 'provides global #delete route' do
+      expect(delete: '/').to route_to('hangar/records#delete')
+    end
   end
 
-  it 'provides #new route' do
-    expect(get: '/factories/posts/new').to route_to('hangar/resources#new')
-  end
+  context 'when route has a namespace' do
+    before do
+      Hangar.route_namespace = :factories
+      Rails.application.reload_routes!
+    end
 
-  it 'provides global #delete route' do
-    expect(delete: '/factories').to route_to('hangar/records#delete')
+    after do
+      Hangar.route_namespace = nil
+      Rails.application.reload_routes!
+    end
+
+    it 'provides namspaced #create route' do
+      expect(post: '/factories/posts').to route_to('hangar/resources#create')
+    end
+
+    it 'provides namspaced #new route' do
+      expect(get: '/factories/posts/new').to route_to('hangar/resources#new')
+    end
+
+    it 'provides global namspaced #delete route' do
+      expect(delete: '/factories').to route_to('hangar/records#delete')
+    end
   end
 end
