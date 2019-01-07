ddee5b4e8428920baebb66966fef9319c456cc95
Brian <brian.rieck@homeadvisor.com>
2019-01-07 09:34:15 -0500

    make routes singular

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   constraints Hangar::RouteConstraint.new do
     scope Hangar.route_namespace do
-      FactoryBot.factories.map { |factory| factory.name.to_s.pluralize.to_sym }.each do |factory|
+      FactoryBot.factories.map { |factory| factory.name.to_s.to_sym }.each do |factory|
         resources factory, only: [:new, :create], controller: 'resources'
       end
       delete '/', to: 'records#delete'
